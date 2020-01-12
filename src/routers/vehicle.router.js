1ef55b832642a44b0fa815e11a607a98fa1cc0b3
Marcelo Magalhães <marcelof03@hotmail.com>
2020-01-12 14:06:27 -0300

    changing server and routers

--- src/routers/vehicle.router.js
+++ src/routers/vehicle.router.js
@@ -10,17 +10,17 @@ const {
   reviewByBrand
 } = require('../controllers/vehicle.controller');
 
-const app = express();
+const router = express.Router()
 
-app.get('/vehicles', async (req, res) => find(req, res));
-app.get('/vehicle/:plate', async (req, res) => findByPlate(req, res));
-app.post('/vehicle', async (req, res) => create(req, res));
-app.patch('/vehicle/:plate', async (req, res) => update(req, res));
-app.delete('/vehicle/:plate', async (req, res) => deleteVehicle(req, res));
+router.get('/vehicles', async (req, res) => find(req, res));
+router.get('/vehicle/:plate', async (req, res) => findByPlate(req, res));
+router.post('/vehicle', async (req, res) => create(req, res));
+router.patch('/vehicle/:plate', async (req, res) => update(req, res));
+router.delete('/vehicle/:plate', async (req, res) => deleteVehicle(req, res));
 
-app.get('/vehicles/review/:brand', async (req, res) => reviewByBrand(req, res));
+router.get('/vehicles/review/:brand', async (req, res) => reviewByBrand(req, res));
 
-app.get('/vehicle/review/:plate', async (req, res) => reviewByPlate(req, res));
-app.patch('/vehicle/review/:plate', async (req, res) => addReview(req, res));
+router.get('/vehicle/review/:plate', async (req, res) => reviewByPlate(req, res));
+router.patch('/vehicle/review/:plate', async (req, res) => addReview(req, res));
 
-module.exports = app;
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const vehicleRouter = require('./routers/vehicle.router');
 const uri = require('./uri');
-
-const app = express();
-app.use(express.json());
+const vehicleRouter = require('./routers/vehicle.router');
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
-app.listen(3000, () => console.log(`Server is running... on http://localhost:3000/`));
-
+const app = express();
+app.use(express.json());
 app.use(vehicleRouter);
 
 app.get('/', async (req, res) => {
@@ -19,3 +16,5 @@ app.get('/', async (req, res) => {
     res.status(500).send(err);
   }
 });
+
+app.listen(3000, () => console.log(`Server is running... on http://localhost:3000/`));
